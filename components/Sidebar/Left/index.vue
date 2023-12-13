b604945095e1b5d2d261373334e824670fceacdc
Kacper Baranowski <kacper.baranowski00@gmail.com>
2023-12-13 02:03:00 +0100

    wykonanie lewego paska bocznego, light / dark mode

---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+import { HomeIcon } from "@heroicons/vue/24/solid"
+import { HashtagIcon, BellIcon, InboxIcon, BookmarkIcon, DocumentTextIcon, UserIcon, EllipsisHorizontalIcon, PencilIcon, ChevronDownIcon } from "@heroicons/vue/24/outline"
+
+const { defaultTransition } = useTailwindConfig()
+</script>
+
+<template>
+ <div class="flex flex-col h-screen">
+
+   <div class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20">
+      <nuxt-link to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </nuxt-link>
+   </div>
+
+   <div class="mt-2 space-y-2">
+
+     <SidebarLeftTab active>
+       <template #icon>
+         <HomeIcon />
+       </template>
+       <template #name>
+         Home
+       </template>
+     </SidebarLeftTab>
+
+     <SidebarLeftTab>
+       <template #icon>
+         <HashtagIcon />
+       </template>
+       <template #name>
+         Explore
+       </template>
+     </SidebarLeftTab>
+
+
+     <SidebarLeftTab>
+       <template #icon>
+         <BellIcon />
+       </template>
+       <template #name>
+         Notifications
+       </template>
+     </SidebarLeftTab>
+
+     <SidebarLeftTab>
+       <template #icon>
+         <InboxIcon />
+       </template>
+       <template #name>
+         Messages
+       </template>
+     </SidebarLeftTab>
+
+     <SidebarLeftTab>
+       <template #icon>
+         <BookmarkIcon />
+       </template>
+       <template #name>
+         Bookmarks
+       </template>
+     </SidebarLeftTab>
+
+     <SidebarLeftTab>
+       <template #icon>
+         <DocumentTextIcon />
+       </template>
+       <template #name>
+         Lists
+       </template>
+     </SidebarLeftTab>
+
+     <SidebarLeftTab>
+       <template #icon>
+         <UserIcon />
+       </template>
+       <template #name>
+         Profile
+       </template>
+     </SidebarLeftTab>
+
+     <SidebarLeftTab>
+       <template #icon>
+         <EllipsisHorizontalIcon />
+       </template>
+       <template #name>
+         More
+       </template>
+     </SidebarLeftTab>
+
+   </div>
+
+ </div>
+</template>
+
+<style scoped>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [],
-  darkmode: 'class',
   theme: {
     screens: {
       xs: "614px",
